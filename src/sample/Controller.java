fa9a1c35bdae7c779e39b958ab23275bb290cb18
billytrinh <quanghoa.trinh@gmail.com>
2021-03-22 11:08:37 +0700

    change screen fx

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,36 +1,16 @@
 package sample;
 
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
-
-import java.util.ArrayList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Controller {
-    public TextField tenSV;
-    public TextField email;
-    public TextArea ds;
-    public Text txtValidate;
 
-    private ArrayList<String> danhSachSV = new ArrayList<>();
-
-    public void submit(){
-        String n =  tenSV.getText();
-        String e = email.getText();
-        if(!n.isEmpty() && !e.isEmpty()){
-            danhSachSV.add(n+"--"+e);
-            String txt  ="";
-            for(String s:danhSachSV){
-                txt += s+"\n";
-            }
-            tenSV.setText("");
-            email.setText("");
-            txtValidate.setText("");
-            ds.setText(txt);
-        }else {
-            txtValidate.setText("Vui lòng nhập email và tên");
-            txtValidate.setDisable(false);
-        }
-
+    public void screenDS() throws Exception{
+//        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("asm1.fxml"));
+        Main.mainStage.setTitle("Danh sách sinh viên");
+        Main.mainStage.setScene(new Scene(root, 600, 700));
+        Main.mainStage.show();
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,12 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    public static Stage mainStage;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("asm1.fxml"));
+        mainStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
         primaryStage.setTitle("Phần mềm gì đó...");
-        primaryStage.setScene(new Scene(root, 600, 700));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
